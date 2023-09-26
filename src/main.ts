111bd7cd0ba8170c98046f11f5465b4cf645d9a2
zhubingxin <zhubingxin@marsgis.cn>
2023-09-26 17:39:07 +0800

    纹理写入深度

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
-import { Engine3D, Scene3D, Object3D, Camera3D, LitMaterial, BoxGeometry, MeshRenderer, DirectLight, HoverCameraController, Color, Vector3, AtmosphericComponent, View3D, ComponentBase, SphereGeometry, InstanceDrawComponent, TAAPost, PostProcessingComponent, Matrix4 } from '@orillusion/core'
-import { CesiumScene } from './ori_map/Scene/CesiumScene'
-import { Stats } from "@orillusion/stats"
-import { UrlTemplateImageryProvider } from './ori_map/Scene/UrlTemplateImageryProvider';
-import { TileCoordinatesImageryProvider } from './ori_map/Scene/TileCoordinatesImageryProvider';
-import { WebMapTileServiceImageryProvider } from './ori_map/Scene/WebMapTileServiceImageryProvider';
-import { GeographicTilingScheme } from './ori_map/Core/GeographicTilingScheme';
+import {
+  Engine3D,
+  Scene3D,
+  Object3D,
+  Camera3D,
+  LitMaterial,
+  BoxGeometry,
+  MeshRenderer,
+  DirectLight,
+  HoverCameraController,
+  Color,
+  Vector3,
+  AtmosphericComponent,
+  View3D,
+  ComponentBase,
+  SphereGeometry,
+  InstanceDrawComponent,
+  TAAPost,
+  PostProcessingComponent,
+  Matrix4
+} from "@orillusion/core";
+import { CesiumScene } from "./ori_map/Scene/CesiumScene";
+import { Stats } from "@orillusion/stats";
+import { UrlTemplateImageryProvider } from "./ori_map/Scene/UrlTemplateImageryProvider";
+import { TileCoordinatesImageryProvider } from "./ori_map/Scene/TileCoordinatesImageryProvider";
+import { WebMapTileServiceImageryProvider } from "./ori_map/Scene/WebMapTileServiceImageryProvider";
+import { GeographicTilingScheme } from "./ori_map/Core/GeographicTilingScheme";
 // 引擎全局配置设置
 //@ts-ignore
 Engine3D.setting.render.postProcessing.taa.jitterSeedCount = 8;
@@ -17,16 +37,16 @@ Engine3D.setting.render.postProcessing.taa.sharpPreBlurFactor = 0.5;
 //@ts-ignore
 Engine3D.setting.render.postProcessing.taa.temporalJitterScale = 0.6;
 
-
-Engine3D.setting.shadow.shadowSize = 2048
+Engine3D.setting.shadow.shadowSize = 2048;
 Engine3D.setting.shadow.shadowBound = 1000;
 Engine3D.setting.shadow.shadowBias = 0.0005;
 
-
-await Engine3D.init({canvasConfig:{
-
-  devicePixelRatio: 1 // 渲染 DPR, 默认使用 window.devicePixelRatio
-}});
+await Engine3D.init({
+  canvasConfig: {
+    devicePixelRatio: 1 // 渲染 DPR, 默认使用 window.devicePixelRatio
+  }
+});
+window.Engine3D = Engine3D;
 //@ts-ignore
 window.mat = new Matrix4();
 //@ts-ignore
@@ -35,9 +55,8 @@ window.vm = new Matrix4();
 
 window.scaleAndBias = new Matrix4();
 
-
 //@ts-ignore
-let scene3D: CesiumScene = window.scene = new CesiumScene({})
+let scene3D: CesiumScene = (window.scene = new CesiumScene({}));
 // let cameraObj: Object3D = new Object3D()
 // let camera = cameraObj.addComponent(Camera3D)
 // camera.perspective(60, Engine3D.aspect, 1, 5000.0)
@@ -47,39 +66,39 @@ let scene3D: CesiumScene = window.scene = new CesiumScene({})
 scene3D.addComponent(Stats);
 
 // create light
-let light: Object3D = new Object3D()
+let light: Object3D = new Object3D();
 // add direct light component
-let component: DirectLight = light.addComponent(DirectLight)
+let component: DirectLight = light.addComponent(DirectLight);
 // adjust lighting
-light.rotationX = 45
-light.rotationY = 70
-component.intensity = 100
+light.rotationX = 45;
+light.rotationY = 70;
+component.intensity = 100;
 // add light object
-scene3D.addChild(light)
-// create new object  
+scene3D.addChild(light);
+// create new object
 class ControllerUpdate extends ComponentBase {
   public onUpdate() {
     // update lifecycle codes
-    scene3D.render()
-    scene3D.screenSpaceCameraController.update()
+    scene3D.render();
+    scene3D.screenSpaceCameraController.update();
   }
 }
 scene3D._renderCollection.addComponent(InstanceDrawComponent);
 
 scene3D.addComponent(ControllerUpdate);
-const Sphere: Object3D = new Object3D()
+const Sphere: Object3D = new Object3D();
 // add MeshRenderer to the object
-let mrs: MeshRenderer = Sphere.addComponent(MeshRenderer)
+let mrs: MeshRenderer = Sphere.addComponent(MeshRenderer);
 // set a box geometry
-mrs.geometry = new SphereGeometry(6378137, 50, 50)
+mrs.geometry = new SphereGeometry(6378137, 50, 50);
 // set a pbr lit material
 mrs.material = new LitMaterial();
 
 // let texture = await Engine3D.res.loadTexture('http://127.0.0.1:5173/earth.jpg');
 // mrs.material.baseMap = texture;
-Sphere.transform.x = 0
-Sphere.transform.y = 0
-Sphere.transform.z = 0
+Sphere.transform.x = 0;
+Sphere.transform.y = 0;
+Sphere.transform.z = 0;
 Sphere.transform.rotationX += 180;
 
 // Sphere.transform.rotationY += 90;
@@ -98,54 +117,52 @@ rencoll.addChild(Sphere);
 // scene3D.imageryLayers.addImageryProvider(
 //   urlTemplateImageryProvide
 // );
-const mapToken = '39d358c825ec7e59142958656c0a6864';// 盈嘉企业开发者秘钥
+const mapToken = "39d358c825ec7e59142958656c0a6864"; // 盈嘉企业开发者秘钥
 
 //https://t4.tianditu.gov.cn/img_c/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=c&FORMAT=tiles&TILECOL={TileCol}&TILEROW={TileRow}&TILEMATRIX={TileMatrix}&tk=75f0434f240669f4a2df6359275146d2
-scene3D.imageryLayers.addImageryProvider(new WebMapTileServiceImageryProvider({
-  // url: 'https://{s}.tianditu.gov.cn/img_w/wmts?SERVICE=WMTS&REQUEST=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&tk=' + mapToken,
-  url: 'https://t4.tianditu.gov.cn/img_c/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=c&FORMAT=tiles&TILECOL={TileCol}&TILEROW={TileRow}&TILEMATRIX={TileMatrix}&tk=' + mapToken,
-  subdomains: ['t0', 't1', 't2', 't3', 't4', 't5', 't6', 't7'],
-  maximumLevel: 17, // 定义最大缩放级别
-  layer: 'tdtImgLayer',
-  style: 'default',
-  format: 'image/jpeg',
-  tilingScheme: new GeographicTilingScheme({
-    numberOfLevelZeroTilesX: 2,
-    numberOfLevelZeroTilesY: 1
-  }),
-  tileMatrixSetID: "EPSG:4326", // 使用谷歌的瓦片切片方式
-}));
+scene3D.imageryLayers.addImageryProvider(
+  new WebMapTileServiceImageryProvider({
+    // url: 'https://{s}.tianditu.gov.cn/img_w/wmts?SERVICE=WMTS&REQUEST=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&tk=' + mapToken,
+    url:
+      "https://t4.tianditu.gov.cn/img_c/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=c&FORMAT=tiles&TILECOL={TileCol}&TILEROW={TileRow}&TILEMATRIX={TileMatrix}&tk=" +
+      mapToken,
+    subdomains: ["t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7"],
+    maximumLevel: 17, // 定义最大缩放级别
+    layer: "tdtImgLayer",
+    style: "default",
+    format: "image/jpeg",
+    tilingScheme: new GeographicTilingScheme({
+      numberOfLevelZeroTilesX: 2,
+      numberOfLevelZeroTilesY: 1
+    }),
+    tileMatrixSetID: "EPSG:4326" // 使用谷歌的瓦片切片方式
+  })
+);
 // scene3D.imageryLayers.addImageryProvider(new (TileCoordinatesImageryProvider as any)());
 
 // scene3D.addChild(rencoll)
 // create a object
-const obj: Object3D = new Object3D()
+const obj: Object3D = new Object3D();
 // add MeshRenderer to the object
-let mr: MeshRenderer = obj.addComponent(MeshRenderer)
+let mr: MeshRenderer = obj.addComponent(MeshRenderer);
 // set a box geometry
-mr.geometry = new BoxGeometry(5, 2, 3)
+mr.geometry = new BoxGeometry(5, 2, 3);
 // set a pbr lit material
-mr.material = new LitMaterial()
+mr.material = new LitMaterial();
 // set location and rotation
-obj.localPosition = new Vector3(0, 0, 0)
-obj.localRotation = new Vector3(0, 45, 0)
-scene3D.addChild(obj)
+obj.localPosition = new Vector3(0, 0, 0);
+obj.localRotation = new Vector3(0, 45, 0);
+scene3D.addChild(obj);
 
 // add an Atmospheric sky enviroment
-scene3D.addComponent(AtmosphericComponent).sunY = 0.6
+scene3D.addComponent(AtmosphericComponent).sunY = 0.6;
 // create a view with target scene and camera
 //@ts-ignore
-let view = window.view = new View3D()
-view.scene = scene3D
+let view = (window.view = new View3D());
+view.scene = scene3D;
 view.camera = scene3D.cameraObj._camera;
 // start render
 Engine3D.startRenderView(view);
-
-
-
-
-
-
 
 // 添加后处理组件
 let postProcessing = scene3D.addComponent(PostProcessingComponent);
--- src/ori_map/Material/TileMaterial copy 2.ts
+++ src/ori_map/Material/TileMaterial copy 2.ts
@@ -1,230 +1,236 @@
-
-import { Color, Engine3D, Material, Matrix4, MorphTarget_shader, RenderShader, ShaderLib, Texture, UUID, UnLit, UnLitMaterial, UniformGPUBuffer, Vector2, Vector3, Vector4 } from '@orillusion/core';
-import { defined } from '../Core/defined';
-import { TileMaterialShader } from './TileMaterialShader';
-
-
-
+import {
+  Color,
+  Engine3D,
+  Material,
+  Matrix4,
+  MorphTarget_shader,
+  RenderShader,
+  ShaderLib,
+  Texture,
+  UUID,
+  UnLit,
+  UnLitMaterial,
+  UniformGPUBuffer,
+  Vector2,
+  Vector3,
+  Vector4
+} from "@orillusion/core";
+import { defined } from "../Core/defined";
+import { TileMaterialShader } from "./TileMaterialShader";
 
 class TileMaterial2 extends Material {
-    defines: any;
-    uniforms: any;
-    modifiedModelView: UniformGPUBuffer;
-    u_dayTextureT: UniformGPUBuffer;
-    shader: RenderShader;
+  defines: any;
+  uniforms: any;
+  modifiedModelView: UniformGPUBuffer;
+  u_dayTextureT: UniformGPUBuffer;
+  shader: RenderShader;
 
-    /**
-     *@constructor
-     */
-    constructor(parameters = {}, shaderSetOptions?: any) {
-        super();
-        // console.log();
-        this.modifiedModelView = new UniformGPUBuffer(128);
-        this.u_dayTextureT = new UniformGPUBuffer(128);
-        this.defines = {};
-        this.uniforms = {
-            u_dayTextures: { value: [] },
-            u_dayTextureTranslationAndScale: { value: [] },
-            u_dayTextureTexCoordsRectangle: { value: [] },
-            u_initialColor: { value: new Vector4(0, 0, 0.5, 1) },
-            u_tileRectangle: { value: new Vector4() },
-            rtc: { value: new Vector3() },
-            u_minMaxHeight: { value: new Vector2() },
-            u_scaleAndBias: { value: new Matrix4() },
-            u_center3D: { value: new Vector3() },
-            u_modifiedModelView: { value: new Matrix4() },
-            u_modifiedModelViewProjection: { value: new Matrix4() }
-        };
-        this.defines.TEXTURE_UNITS = shaderSetOptions.numberOfDayTextures;
-        // this.glslVersion = GLSL3;
-        this.defines.APPLY_GAMMA = '';
+  /**
+   *@constructor
+   */
+  constructor(parameters = {}, shaderSetOptions?: any) {
+    super();
+    // console.log();
+    this.modifiedModelView = new UniformGPUBuffer(128);
+    this.u_dayTextureT = new UniformGPUBuffer(128);
 
-        this.defines.GROUND_ATMOSPHERE = '';
-        let shaderName = 'daytexteure_' + shaderSetOptions.numberOfDayTextures;
-        ShaderLib.register(shaderName, this.getwgsl(shaderSetOptions.numberOfDayTextures));
+    this.defines = {};
+    this.uniforms = {
+      u_dayTextures: { value: [] },
+      u_dayTextureTranslationAndScale: { value: [] },
+      u_dayTextureTexCoordsRectangle: { value: [] },
+      u_initialColor: { value: new Vector4(0, 0, 0.5, 1) },
+      u_tileRectangle: { value: new Vector4() },
+      rtc: { value: new Vector3() },
+      u_minMaxHeight: { value: new Vector2() },
+      u_scaleAndBias: { value: new Matrix4() },
+      u_center3D: { value: new Vector3() },
+      u_modifiedModelView: { value: new Matrix4() },
+      u_modifiedModelViewProjection: { value: new Matrix4() }
+    };
+    this.defines.TEXTURE_UNITS = shaderSetOptions.numberOfDayTextures;
+    // this.glslVersion = GLSL3;
+    this.defines.APPLY_GAMMA = "";
 
-        let shader = this.shader = new RenderShader(shaderName, shaderName);
-        shader.setShaderEntry(`VertMain`, `FragMain`)
+    this.defines.GROUND_ATMOSPHERE = "";
+    let shaderName = "daytexteure_" + shaderSetOptions.numberOfDayTextures;
+    ShaderLib.register(shaderName, this.getwgsl(shaderSetOptions.numberOfDayTextures));
 
-        shader.setUniformVector4(`transformUV1`, new Vector4(0, 0, 1, 1));
-        shader.setUniformVector4(`transformUV2`, new Vector4(0, 0, 1, 1));
-        shader.setUniformColor(`baseColor`, new Color());
-        shader.setUniformFloat(`alphaCutoff`, 0.5);
-        this.modifiedModelView.setMatrix('matrixMVP_RTE', Matrix4.help_matrix_0);
+    let shader = (this.shader = new RenderShader(shaderName, shaderName));
+    shader.setDefine("USE_OUTDEPTH", true);
+    shader.setShaderEntry(`VertMain`, `FragMain`);
 
-        // this.modifiedModelView.apply();
-        shader.setUniformBuffer(`modifiedModelView`, this.modifiedModelView);
-        shader.setUniformBuffer(`u_dayTextureT`, this.u_dayTextureT);
-        let shaderState = shader.shaderState;
-        shaderState.acceptShadow = false;
-        shaderState.receiveEnv = false;
-        shaderState.acceptGI = false;
-        shaderState.useLight = false;
+    shader.setUniformVector4(`transformUV1`, new Vector4(0, 0, 1, 1));
+    shader.setUniformVector4(`transformUV2`, new Vector4(0, 0, 1, 1));
+    shader.setUniformColor(`baseColor`, new Color());
+    shader.setUniformFloat(`alphaCutoff`, 0.5);
+    this.modifiedModelView.setMatrix("matrixMVP_RTE", Matrix4.help_matrix_0);
 
-        shader.setUniformColor("ccc", new Color(1.0, 0.0, 0.0, 1.0));
-        // create a image material
-        // let mat = new ImageMaterial()
-        // mat.baseMap = texture
-        // default value
-        // this.baseMap = Engine3D.res.whiteTexture;
-        this.defaultPass = this.shader
+    // this.modifiedModelView.apply();
+    shader.setUniformBuffer(`modifiedModelView`, this.modifiedModelView);
+    shader.setUniformBuffer(`u_dayTextureT`, this.u_dayTextureT);
+    let shaderState = shader.shaderState;
+    shaderState.acceptShadow = false;
+    shaderState.receiveEnv = false;
+    shaderState.acceptGI = false;
+    shaderState.useLight = false;
+
+    shader.setUniformColor("ccc", new Color(1.0, 0.0, 0.0, 1.0));
+    // create a image material
+    // let mat = new ImageMaterial()
+    // mat.baseMap = texture
+    // default value
+    // this.baseMap = Engine3D.res.whiteTexture;
+    this.defaultPass = this.shader;
+  }
+
+  get dayTextures(): Texture[] {
+    return this.uniforms.u_dayTextures.value;
+  }
+
+  set dayTextures(value) {
+    if (!defined(value)) {
+      return;
+    }
+    this.uniforms.u_dayTextures.value = value;
+    for (let index = 0; index < value.length; index++) {
+      const element = value[index];
+      this.shader.setTexture(`u_dayTextures${index}`, element);
+    }
+  }
+
+  get minMaxHeight() {
+    return this.uniforms.u_minMaxHeight;
+  }
+
+  set minMaxHeight(res) {
+    this.uniforms.u_minMaxHeight = res;
+    this.modifiedModelView.setVector2("u_minMaxHeight", res);
+    this.modifiedModelView.apply();
+  }
+
+  get scaleAndBias() {
+    return this.uniforms.u_scaleAndBias;
+  }
+
+  set scaleAndBias(res) {
+    this.uniforms.u_scaleAndBias = res;
+    // this.shader.set('u_minMaxHeight',res);
+    this.modifiedModelView.setMatrix("u_scaleAndBias", res);
+
+    this.modifiedModelView.apply();
+  }
+
+  get dayTextureTranslationAndScale() {
+    return this.uniforms.u_dayTextureTranslationAndScale.value;
+  }
+
+  set dayTextureTranslationAndScale(value) {
+    if (!defined(value)) {
+      return;
+    }
+    this.uniforms.u_dayTextureTranslationAndScale.value = value;
+    for (let index = 0; index < value.length; index++) {
+      const element = value[index];
+      this.u_dayTextureT.setVector4(`u_dayTextureTranslationAndScale${index}`, element);
     }
 
-    get dayTextures(): Texture[] {
-        return this.uniforms.u_dayTextures.value;
+    this.u_dayTextureT.apply();
+  }
+
+  get dayTextureTexCoordsRectangle() {
+    return this.uniforms.u_dayTextureTexCoordsRectangle.value;
+  }
+
+  set dayTextureTexCoordsRectangle(value) {
+    if (!defined(value)) {
+      return;
+    }
+    this.uniforms.u_dayTextureTexCoordsRectangle.value = value;
+    for (let index = 0; index < value.length; index++) {
+      const element = value[index];
+      this.u_dayTextureT.setVector4(`u_dayTextureTexCoordsRectangle${index}`, element);
     }
 
-    set dayTextures(value) {
-        if (!defined(value)) {
-            return;
-        }
-        this.uniforms.u_dayTextures.value = value;
-        for (let index = 0; index < value.length; index++) {
-            const element = value[index];
-            this.shader.setTexture(`u_dayTextures${index}`, element)
+    this.u_dayTextureT.apply();
+  }
 
-        }
+  get initialColor() {
+    return this.uniforms.u_initialColor.value;
+  }
+
+  set initialColor(value) {
+    if (!defined(value)) {
+      return;
     }
+    this.uniforms.u_initialColor.value.copy(value);
+  }
 
-    get minMaxHeight(){
-        return this.uniforms.u_minMaxHeight;
-    }
+  /**
+   * Set material environment map
+   */
+  public set envMap(texture: Texture) {
+    //not need env texture
+  }
 
-    set minMaxHeight( res){
-        this.uniforms.u_minMaxHeight = res;
-        this.modifiedModelView.setVector2('u_minMaxHeight',res);
-        this.modifiedModelView.apply();
-    }
+  /**
+   * Set material shadow map
+   */
+  public set shadowMap(texture: Texture) {
+    //not need shadowMap texture
+  }
 
-    get scaleAndBias(){
-        return this.uniforms.u_scaleAndBias;
-    }
+  get QUANTIZATION_BITS12() {
+    return this.uniforms.QUANTIZATION_BITS12;
+  }
 
-    set scaleAndBias(res){
-        this.uniforms.u_scaleAndBias = res;
-        // this.shader.set('u_minMaxHeight',res);
-        this.modifiedModelView.setMatrix('u_scaleAndBias',res);
+  set QUANTIZATION_BITS12(res: boolean) {
+    this.uniforms.QUANTIZATION_BITS12 = res;
+    this.shader.setDefine("QUANTIZATION_BITS12", res);
+  }
 
-        this.modifiedModelView.apply();
+  getwgsl(num: number) {
+    let ddf = ``;
 
-
-    }
-
-
-    get dayTextureTranslationAndScale() {
-
-        return this.uniforms.u_dayTextureTranslationAndScale.value;
-    }
-
-    set dayTextureTranslationAndScale(value) {
-        if (!defined(value)) {
-            return;
-        }
-        this.uniforms.u_dayTextureTranslationAndScale.value = value;
-        for (let index = 0; index < value.length; index++) {
-            const element = value[index];
-            this.u_dayTextureT.setVector4(`u_dayTextureTranslationAndScale${index}`, element)
-        }
-
-        this.u_dayTextureT.apply();
-    }
-
-    get dayTextureTexCoordsRectangle() {
-        return this.uniforms.u_dayTextureTexCoordsRectangle.value;
-    }
-
-    set dayTextureTexCoordsRectangle(value) {
-        if (!defined(value)) {
-            return;
-        }
-        this.uniforms.u_dayTextureTexCoordsRectangle.value = value;
-        for (let index = 0; index < value.length; index++) {
-            const element = value[index];
-            this.u_dayTextureT.setVector4(`u_dayTextureTexCoordsRectangle${index}`, element)
-
-        }
-
-        this.u_dayTextureT.apply();
-    }
-
-    get initialColor() {
-        return this.uniforms.u_initialColor.value;
-    }
-
-    set initialColor(value) {
-        if (!defined(value)) {
-            return;
-        }
-        this.uniforms.u_initialColor.value.copy(value);
-    }
-
-    /**
-     * Set material environment map
-     */
-    public set envMap(texture: Texture) {
-        //not need env texture
-    }
-
-    /**
-     * Set material shadow map
-     */
-    public set shadowMap(texture: Texture) {
-        //not need shadowMap texture
-    }
-
-    get QUANTIZATION_BITS12(){
-        return this.uniforms.QUANTIZATION_BITS12
-    }
-
-    set QUANTIZATION_BITS12(res:boolean){
-        this.uniforms.QUANTIZATION_BITS12 = res;
-        this.shader.setDefine('QUANTIZATION_BITS12',res)
-    }
-
-    getwgsl(num: number) {
-        let ddf = ``;
-
-        for (let index = 0; index < num; index++) {
-            ddf += /*wgsl*/ `
+    for (let index = 0; index < num; index++) {
+      ddf += /*wgsl*/ `
             u_dayTextureTranslationAndScale${index}:vec4<f32>,
             u_dayTextureTexCoordsRectangle${index}:vec4<f32>,
-            `
-        }
-        // var u_dayTextures${index}: texture_2d<f32>;
-        // var u_dayTextureTexCoordsRectangle${index}: vec4<f32>;
-        let uniform = /*wgsl*/`
+            `;
+    }
+    // var u_dayTextures${index}: texture_2d<f32>;
+    // var u_dayTextureTexCoordsRectangle${index}: vec4<f32>;
+    let uniform = /*wgsl*/ `
         struct DayTextureT {
             ${ddf}
         };
         @group(3) @binding(0)
         var<uniform> u_dayTextureT:  DayTextureT;
-        `
+        `;
 
-        for (let index = 0; index < num; index++) {
-            let number = []
-            for (let j = 0; j < 2; j++) {
-                let n = index * 2 + j + 1;
-                number.push(n);
-            }
-            uniform += /*wgsl*/`
+    for (let index = 0; index < num; index++) {
+      let number = [];
+      for (let j = 0; j < 2; j++) {
+        let n = index * 2 + j + 1;
+        number.push(n);
+      }
+      uniform += /*wgsl*/ `
             @group(3) @binding(${number[0]})
             var u_dayTextures${index}Sampler: sampler;
             @group(3) @binding(${number[1]})
             var u_dayTextures${index}: texture_2d<f32>;
-           `
-        }
+           `;
+    }
 
-
-
-        let sdf = ``
-        for (let i = 0; i < num; i++) {
-            let str = ''
-            // if(dayTextureUseWebMercatorT[i]){
-            //     str = `tileTextureCoordinates = v_textureCoordinates.xz;`
-            // }else{
-            //     str = `tileTextureCoordinates = v_textureCoordinates.xy;`
-            // }
-            sdf += `
+    let sdf = ``;
+    for (let i = 0; i < num; i++) {
+      let str = "";
+      // if(dayTextureUseWebMercatorT[i]){
+      //     str = `tileTextureCoordinates = v_textureCoordinates.xz;`
+      // }else{
+      //     str = `tileTextureCoordinates = v_textureCoordinates.xy;`
+      // }
+      sdf += `
 
                 var textureAlpha${i} = 1.0;
 
@@ -252,12 +258,10 @@ class TileMaterial2 extends Material {
                     previousColor = vec4<f32>(outColor${i}, outAlpha${i});
 
                 }
-            `
-        }
+            `;
+    }
 
-
-
-        let wgsl = /*wgsl*/ `
+    let wgsl = /*wgsl*/ `
         #include "GlobalUniform"
         #include "WorldMatrixUniform"
         
@@ -309,6 +313,16 @@ class TileMaterial2 extends Material {
         return z;
        }
 
+       fn applyLogarithmicDepth2(
+        clipPosition: vec4<f32>,
+        logarithmicDepthConstant: f32,
+        perspectiveFarPlaneDistance: f32) -> f32
+   {
+    let z = ((2.0 * log((logarithmicDepthConstant * clipPosition.z) + 1.0) / 
+                      log((logarithmicDepthConstant * perspectiveFarPlaneDistance) + 1.0)) - 1.0) * clipPosition.w;
+    return z;
+   }
+
         @vertex
         fn VertMain( in: VertexInput ) -> VertexOutput {
          
@@ -331,7 +345,7 @@ class TileMaterial2 extends Material {
 
             #endif
             out.depthBuf = applyLogarithmicDepth(clipPosition,0.1,10000000000.0);
-            out.member = vec4<f32>(clipPosition.x,clipPosition.y,out.depthBuf,clipPosition.w);
+            out.member = vec4<f32>(clipPosition.x,clipPosition.y,applyLogarithmicDepth(clipPosition,0.1,10000000000.0),clipPosition.w);
 
             return out;
         }
@@ -353,6 +367,7 @@ class TileMaterial2 extends Material {
                 @location(2) worldNormal: vec4<f32>,
                 @location(3) material: vec4<f32>,
             #endif
+            @builtin(frag_depth) out_depth: f32
         };
 
         @group(1) @binding(0)
@@ -369,48 +384,45 @@ class TileMaterial2 extends Material {
             var previousColor = vec4<f32>(0.0, 0.0, 0.5, 1.0);
          
             ${sdf}
-
-            
             
             out.color = previousColor;
+            out.out_depth = in.depthBuf;
             return out;
         }
         
         
-        `
-        return wgsl
-    }
+        `;
+    return wgsl;
+  }
 
+  public clone(): this {
+    // console.log(`clone LitMaterial ${this.name}`);
 
-    public clone(): this {
-        // console.log(`clone LitMaterial ${this.name}`);
+    let ret = new UnLitMaterial();
+    ret.baseMap = this.baseMap;
+    ret.normalMap = this.normalMap;
+    ret.emissiveMap = this.emissiveMap;
+    this.uvTransform_1 && (ret.uvTransform_1 = new Vector4().copyFrom(this.uvTransform_1));
+    this.uvTransform_2 && (ret.uvTransform_2 = new Vector4().copyFrom(this.uvTransform_2));
+    ret.baseColor = this.baseColor.clone();
+    ret.emissiveColor = this.emissiveColor.clone();
+    ret.envIntensity = this.envIntensity;
+    ret.normalScale = this.normalScale;
+    ret.emissiveIntensity = this.emissiveIntensity;
+    ret.alphaCutoff = this.alphaCutoff;
 
-        let ret = new UnLitMaterial();
-        ret.baseMap = this.baseMap;
-        ret.normalMap = this.normalMap;
-        ret.emissiveMap = this.emissiveMap;
-        this.uvTransform_1 && (ret.uvTransform_1 = new Vector4().copyFrom(this.uvTransform_1));
-        this.uvTransform_2 && (ret.uvTransform_2 = new Vector4().copyFrom(this.uvTransform_2));
-        ret.baseColor = this.baseColor.clone();
-        ret.emissiveColor = this.emissiveColor.clone();
-        ret.envIntensity = this.envIntensity;
-        ret.normalScale = this.normalScale;
-        ret.emissiveIntensity = this.emissiveIntensity;
-        ret.alphaCutoff = this.alphaCutoff;
+    ret.transparent = this.transparent;
+    ret.cullMode = this.cullMode;
+    ret.blendMode = this.blendMode;
 
-        ret.transparent = this.transparent;
-        ret.cullMode = this.cullMode;
-        ret.blendMode = this.blendMode;
+    this.cloneObject(this.shaderState, ret.shaderState);
+    this.cloneObject(this.renderShader.defineValue, ret.renderShader.shaderState);
+    this.cloneObject(this.renderShader.constValues, ret.renderShader.constValues);
 
-        this.cloneObject(this.shaderState, ret.shaderState);
-        this.cloneObject(this.renderShader.defineValue, ret.renderShader.shaderState);
-        this.cloneObject(this.renderShader.constValues, ret.renderShader.constValues);
+    return ret as this;
+  }
 
-        return ret as this;
-    }
-
-    debug() {
-    }
+  debug() {}
 }
 
 export { TileMaterial2 };
